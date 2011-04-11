cc5d12f29e44b12099297687988024b2ed4d2a91
Mathieu Lecarme <mathieu@garambrogne.net>
2011-04-11 22:34:18 +0200

    workers POC

--- lib/worker.php
+++ lib/worker.php
@@ -1,7 +1,29 @@
 <?php
 require 'Predis.php';
+$_REDIS = new Predis_Client();
+
 class Worker {
 	function __construct() {
-		$this->redis = new Predis_Client();
 	}
+	function perform() {
+	}
+	
+}
+
+function async_call($function, $args) {
+	global $_REDIS;
+	$_REDIS->lpush('queue', serialize(array($function, $args)));
+}
+
+function async_work() {
+	global $_REDIS;
+	while(true) {
+		list($liste, $data) = $_REDIS->brpop('queue', 300);
+		$data = unserialize($data);
+		call_user_func_array($data[0], $data[1]);
+	}
+}
+
+if(sizeof($argv) && $argv[1] == 'worker') {
+	async_work();
 }
--- test.php
+++ test.php
@@ -1,5 +1,13 @@
 <?php
 require 'lib/worker.php';
 
-$w = new Worker();
-var_dump($w);
+//$w = new Worker();
+
+function test($time, $text) {
+	sleep($time);
+	echo "$text\n";
+}
+
+for($i=0; $i < 10; $i++) {
+	async_call('test', array(3, "Hello world $i"));
+}
