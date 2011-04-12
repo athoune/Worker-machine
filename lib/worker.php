fcc32775a0eeafc96d11723d3421fab79d3f8e9a
Mathieu Lecarme <mathieu@garambrogne.net>
2011-04-12 10:13:16 +0200

    mapreduce

--- lib/worker.php
+++ lib/worker.php
@@ -15,15 +15,44 @@ function async_call($function, $args) {
 	$_REDIS->lpush('queue', serialize(array($function, $args)));
 }
 
+function batch($function, $largs) {
+	global $_REDIS;
+	$pid = uniqid();//[TODO] uuid later?
+	foreach($largs as $args) {
+		$_REDIS->lpush('queue', serialize(array($function, $args, $pid)));
+	}
+	$pubsub = $_REDIS->pubSubContext();
+	$pubsub->subscribe("pid:$pid");
+	$results = array();
+	$cpt = sizeof($largs);
+	foreach ($pubsub as $message) {
+		if($message->kind == 'message') {
+			$results[] = unserialize($message->payload);
+			$cpt --;
+			print $cpt;
+			if($cpt == 0) {
+				break;
+			}
+		}
+	}
+	unset($pubsub);
+	return $results;
+}
+
 function async_work() {
 	global $_REDIS;
 	while(true) {
-		list($liste, $data) = $_REDIS->brpop('queue', 300);
-		$data = unserialize($data);
-		call_user_func_array($data[0], $data[1]);
+		list($liste, $sdata) = $_REDIS->brpop('queue', 300);
+		$data = unserialize($sdata);
+		$result = call_user_func_array($data[0], $data[1]);
+		if(sizeof($data) >= 3) {
+			$rresult = serialize($result);
+			$_REDIS->lpush("result:$data[2]", $rresult);
+			$_REDIS->publish("pid:$data[2]", $rresult);
+		}
 	}
 }
 
-if(sizeof($argv) && $argv[1] == 'worker') {
+if(sizeof($argv) && $argv[1] == '--worker') {
 	async_work();
 }
