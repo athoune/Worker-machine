0a43a44da2faff53eefba5db99bff3ae918ba643
Mathieu Lecarme <mathieu@garambrogne.net>
2011-04-12 10:14:46 +0200

    silly mapreduce

---
+++ test_mapreduce.php
@@ -0,0 +1,12 @@
+<?php
+require 'lib/worker.php';
+
+function map($data) {
+	echo $data;
+	return crypt($data, '$6$rounds=5000$toto');
+}
+
+if($argv[1] == '--mapreduce') {
+	$crypt = batch('map', array(array('Pim'), array('Pam'), array('Poum')));
+	var_dump($crypt);
+}
