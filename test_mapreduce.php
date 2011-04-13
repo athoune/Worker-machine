ffb0575fefc32b5fbdb4b35d065a8f758a0fbfda
Mathieu Lecarme <mathieu@garambrogne.net>
2011-04-13 21:24:54 +0200

    testing with http crawl

--- test_mapreduce.php
+++ test_mapreduce.php
@@ -1,12 +1,15 @@
 <?php
 require 'lib/worker.php';
 
-function map($data) {
-	echo $data;
-	return crypt($data, '$6$rounds=5000$somesalt');
+function map($url) {
+	echo "$url\n";
+	$html = file_get_contents("http://$url");
+	preg_match('/<title>(.*)<\/title>/i', $html, $matches);
+	return $matches[1];
 }
 
 if($argv[1] == '--mapreduce') {
-	$crypt = batch('map', array(array('Pim'), array('Pam'), array('Poum')));
-	var_dump($crypt);
+	$titles = batch('map', array('linuxfr.org', 'www.slashdot.org', 'www.boingboing.net', 'www.rue89.com', 'blog.makezine.com'));
+	sort($titles);
+	var_dump($titles);
 }
